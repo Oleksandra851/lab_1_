f7554846c7443dd50e0d5213fc0a6da0dfe20263
Oleksandra <bodnariuk.oleksandra@chnu.edu.ua>
2026-09-07 19:52:04 +0300

    Add project files.

---
+++ lab_1_/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace lab_1_
+{
+    public partial class Form1 : Form
+    {
+        private readonly TeacherRepository _repository;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _repository = new TeacherRepository();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                dataGridView1.DataSource = _repository.GetTeachersOverview();
+                dataGridView1.AutoResizeColumns();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка підключення до БД: " + ex.Message,
+                                "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ lab_1_/TeacherRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace lab_1_
+{
+    public class TeacherRepository
+    {
+        private readonly string _connectionString;
+
+        public TeacherRepository()
+        {
+            // Отримуємо connection string із App.config
+            _connectionString = ConfigurationManager.ConnectionStrings["UniversityDb"].ConnectionString;
+        }
+
+        public DataTable GetTeachersOverview()
+        {
+            string query = @"
+                SELECT 
+                    T.TeacherID AS [ID],
+                    T.LastName + ' ' + T.FirstName + ' ' + ISNULL(T.MiddleName, '') AS [ПІБ Викладача],
+                    T.Phone AS [Телефон],
+                    T.Workplace AS [Місце роботи],
+                    P.PositionName AS [Посада],
+                    P.HourlyRate AS [Погодинна ставка],
+                    S.SubjectName AS [Предмет],
+                    TS.HoursRead AS [Прочитані години],
+                    T.HomeAddress AS [Домашня адреса],
+                    T.Characteristic AS [Характеристика]
+                FROM Teachers T
+                INNER JOIN Positions P ON T.PositionID = P.PositionID
+                INNER JOIN TeacherSubjects TS ON T.TeacherID = TS.TeacherID
+                INNER JOIN Subjects S ON TS.SubjectID = S.SubjectID
+                ORDER BY T.LastName;";
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                return dataTable;
+            }
+        }
+    }
+}
